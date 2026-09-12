664acac55fab8b1069d56c9c04e213d3dee3baae
CR1MSEN <2813199072@qq.com>
2026-09-12 19:53:32 +0800

    v1.3

--- MechrevoBatteryManager/MechrevoBatteryManager.Service/Uninstall-MechrevoBatteryManager.cmd
+++ MechrevoBatteryManager/MechrevoBatteryManager.Service/Uninstall-MechrevoBatteryManager.cmd
@@ -8,6 +8,6 @@ if not "%errorlevel%"=="0" (
 )
 sc.exe stop MechrevoBatteryManager >nul 2>&1
 sc.exe delete MechrevoBatteryManager >nul 2>&1
-echo Service removed.
-start "" /b powershell.exe -NoProfile -WindowStyle Hidden -Command "$root='%~dp0'; Start-Sleep -Seconds 2; Get-ChildItem -LiteralPath $root -Force | Where-Object { $_.Name -ne 'log' } | Remove-Item -Recurse -Force"
+echo Service removed. Removing the entire installation directory...
+start "" /b powershell.exe -NoProfile -WindowStyle Hidden -Command "$root='%~dp0'; Start-Sleep -Seconds 2; Remove-Item -LiteralPath $root -Recurse -Force"
 exit /b 0
