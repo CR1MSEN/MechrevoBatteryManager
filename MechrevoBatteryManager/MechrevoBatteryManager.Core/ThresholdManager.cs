2982e27a2dfa741e9b0156f0cb4fff2c12b9138e
CR1MSEN <2813199072@qq.com>
2026-09-12 18:42:31 +0800

    更新检查逻辑

--- MechrevoBatteryManager/MechrevoBatteryManager.Core/ThresholdManager.cs
+++ MechrevoBatteryManager/MechrevoBatteryManager.Core/ThresholdManager.cs
@@ -6,6 +6,8 @@ namespace MechrevoBatteryManager
     {
         public byte BeforeUpper, BeforeLower, AfterUpper, AfterLower;
         public byte ChargeLimitGate;
+        public byte State07C3, State0770, StoredLimit;
+        public bool LimitEnabled;
         public bool Changed;
     }
 
@@ -17,7 +19,11 @@ namespace MechrevoBatteryManager
         public static ThresholdResult Read(string dllPath)
         {
             using (var ec = new EcClient(dllPath))
-                return new ThresholdResult { BeforeUpper = ec.Read(UpperAddress), BeforeLower = ec.Read(LowerAddress), ChargeLimitGate = ec.Read(0x0742) };
+            {
+                var c3 = ec.Read(0x07C3); var w70 = ec.Read(0x0770); var stored = (byte)(ec.Read(0x087F) & 0x7F);
+                var stateEnabled = c3 == 4 || c3 == 5 || w70 == 4 || w70 == 5;
+                return new ThresholdResult { BeforeUpper = ec.Read(UpperAddress), BeforeLower = ec.Read(LowerAddress), ChargeLimitGate = ec.Read(0x0742), State07C3 = c3, State0770 = w70, StoredLimit = stored, LimitEnabled = stateEnabled || (stored >= 1 && stored <= 100) };
+            }
         }
 
         public static ThresholdResult Apply(BatteryConfig config)
